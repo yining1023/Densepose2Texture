0ea17dc0a61025b323d55645e0711fd22e534200
Yining Shi <shiyining1023@gmail.com>
2019-08-29 00:03:37 -0400

    first commit

---
+++ model.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+class Densepose2Texture():
+  def __init__(self, checkpoint_path):
+    self.Tex_Atlas = cv2.imread('./texture.png')[:,:,::-1]/255.
+    self.TextureIm  = np.zeros([24,200,200,3])
+
+    for i in range(4):
+      for j in range(6):
+        self.TextureIm[(6*i+j) , :,:,:] = self.Tex_Atlas[ (200*j):(200*j+200)  , (200*i):(200*i+200) ,: ]
+
+  def TransferTexture(TextureIm,im,IUV):
+    U = IUV[:,:,1]
+    V = IUV[:,:,2]
+
+    R_im = np.zeros(U.shape)
+    G_im = np.zeros(U.shape)
+    B_im = np.zeros(U.shape)
+
+    for PartInd in range(1,25):    ## Set to xrange(1,23) to ignore the face part.
+        tex = TextureIm[PartInd-1,:,:,:].squeeze() # get texture for each part.
+
+        R = tex[:,:,0]
+        G = tex[:,:,1]
+        B = tex[:,:,2]
+
+        x,y = np.where(IUV[:,:,0]==PartInd)
+        u_current_points = U[x,y]   #  Pixels that belong to this specific part.
+        v_current_points = V[x,y]
+
+        r_current_points = R[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        g_current_points = G[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        b_current_points = B[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        ##  Get the RGB values from the texture images.
+        R_im[IUV[:,:,0]==PartInd] = r_current_points
+        G_im[IUV[:,:,0]==PartInd] = g_current_points
+        B_im[IUV[:,:,0]==PartInd] = b_current_points
+    generated_image = np.concatenate((B_im[:,:,np.newaxis],G_im[:,:,np.newaxis],R_im[:,:,np.newaxis]), axis =2 ).astype(np.uint8)
+    BG_MASK = generated_image==0
+    generated_image[BG_MASK] = im[BG_MASK]  ## Set the BG as the old image.
+    return generated_image
+  
+  def generate(self, image):
+    IUV = image #cv2.imread('../DensePoseData/demo_data/synth_UV_example.png')
+    im  = np.zeros(IUV.shape)
+    image = self.TransferTexture(self.TextureIm, im, IUV)
---
+++ runway_model.py
@@ -0,0 +1,30 @@
+import runway
+from model import Densepose2Texture
+
+# The setup() function runs once when the model is initialized, and will run
+# again for each well formed HTTP POST request to http://localhost:9000/setup.
+@runway.setup(options={})
+def setup():
+    return DeepLabModel()
+
+# The @runway.command() decorator is used to create interfaces to call functions
+# remotely via an HTTP endpoint. This lets you send data to, or get data from,
+# your model. Each command creates an HTTP route that the Runway app will use
+# to communicate with your model (e.g. POST /generate). Multiple commands
+# can be defined for the same model.
+@runway.command('generate', inputs={'image': runway.image}, outputs={'image': runway.image}, description='Generate a texture image.')
+def generate(model, inputs):
+    # Functions wrapped by @runway.command() receive two arguments:
+    # 1. Whatever is returned by a function wrapped by @runway.setup(),
+    #    usually a model.
+    # 2. The input arguments sent by the remote caller via HTTP. These values
+    #    match the schema defined by inputs.
+    img = inputs['image']
+    return model.generate(img)
+
+# The runway.run() function triggers a call to the function wrapped by
+# @runway.setup() passing model_options as its single argument. It also
+# creates an HTTP server that listens for and fulfills remote requests that
+# trigger commands.
+if __name__ == '__main__':
+    runway.run(host='0.0.0.0', port=9000, model_options={})
